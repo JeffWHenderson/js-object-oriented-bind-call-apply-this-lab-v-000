621172b9bb6ddeb00de4ad27be7775bf8b1d2bac
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-26 16:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
 
-function setThisWithApply(fn, thisValue, arg){
-  console.log(fn.apply(thisValue, [arg]))
+function setThisWithApply(fn, thisValue, args){
+  console.log(fn.apply(thisValue, [...args]))
 }
  //bob = {name: 'bob'}
 // age = 18
